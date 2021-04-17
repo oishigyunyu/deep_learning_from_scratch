9ba9687b5be1cd417e713b0cded87e27996a1503
oishigyunyu <yamamuuuura@gmail.com>
2021-04-17 16:29:09 +0900

    本家様からcommonを追加

--- src/dataset/mnist.py
+++ src/dataset/mnist.py
@@ -1,15 +1,5 @@
-import numpy as np
 import tensorflow_datasets as tfds
-
-(ds_train, ds_test), info = tfds.load('mnist', split=['train', 'test'], shuffle_files=True,
-                                      as_supervised=True,
-                                      with_info=True
-                                      )
-
-
-print()
-train_img_array = []
-train_label_array = []
+import numpy as np
 
 
 class MNIST():
@@ -21,7 +11,7 @@ class MNIST():
                                                              with_info=True
                                                              )
 
-    def ds_to_array(self, ds, info, split=None):
+    def ds_to_array(self, ds, info, flatten, normalize, one_hot, split=None):
         img_array = []
         label_array = []
         if(split is None):
@@ -33,11 +23,22 @@ class MNIST():
                 label_array.append(label.numpy())
         img_array = np.array(img_array)
         label_array = np.array(label_array)
+        if(flatten):
+            img_array = img_array.reshape(info.splits[split].num_examples, -1)
+        
+        if(normalize):
+            img_array = img_array / 255.0
+        if(one_hot):
+            T = np.zeros((label_array.size, 10))
+            for idx, row in enumerate(T):
+                row[label_array[idx]] = 1
+
+            label_array = T
+
         return (img_array, label_array)
-    
-    def get_dataset(self):
-        ds_train = self.ds_to_array(self.ds_train, self.info, split='train')
-        ds_test = self.ds_to_array(self.ds_train, self.info, split='test')
+
+    def get_dataset(self, flatten, normalize, one_hot):
+        ds_train = self.ds_to_array(self.ds_train, self.info, flatten, normalize, one_hot, split='train')
+        ds_test = self.ds_to_array(self.ds_train, self.info, flatten, normalize, one_hot, split='test')
 
         return ds_train, ds_test
-
